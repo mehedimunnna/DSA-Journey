58df2fd9e397b5b36b8967cabfa6d2c0c19472eb
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-10-26 08:10:12 +0600

    Update 1.1.basic.cpp

--- Arrays/1.1.basic.cpp
+++ Arrays/1.1.basic.cpp
@@ -5,8 +5,9 @@ int main(){
     int a[5];
     a[2] = 4;
     a[4] = 6;
+    a[4]++;                           //now a[4] = 7
     
-    a[0] = a[2]+a[4];                 //a[0] = 10;
+    a[0] = a[2]+a[4];                 //a[0] = 11;
     
     cout<<"Output: "<<a[0]<<endl;      
     return 0;
