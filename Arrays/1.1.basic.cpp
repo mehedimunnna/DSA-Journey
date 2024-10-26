7dc0b3e425d5804a664deed01c08cce9e91ce210
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-10-26 08:22:26 +0600

    Update 1.1.basic.cpp

--- Arrays/1.1.basic.cpp
+++ Arrays/1.1.basic.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main(){
-    int a[5];
-    a[2] = 4;
-    a[4] = 6;
-    a[4]++;                           //now a[4] = 7
+    int a[10];
     
-    a[0] = a[2]+a[4];                 //a[0] = 11;
+    a[2] = 5;
+    a[3] = 10;
+    a[3] += 2;                        //now a[3] = 12
+    a[4] = 15;
+    a[4]++;                           //now a[4] = 16
+    a[5] = 3;
+    a[5]--;                           //now a[5] = 2
+    a[8] = a[2];                      //now a[8] = 5;
     
-    cout<<"Output: "<<a[0]<<endl;      
+    a[0] = a[2]+a[3]+a[4]+a[5]+a[8];            //a[0] = 40;
+    
+    cout<<"Output: "<<a[0]<<endl;   
+    
     return 0;
 }
