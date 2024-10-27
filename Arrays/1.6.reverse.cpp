3bc3024045bfc31cf084e89680892edf583a2c84
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-10-27 14:32:33 +0600

    Update 1.6.reverse.cpp

--- Arrays/1.6.reverse.cpp
+++ Arrays/1.6.reverse.cpp
@@ -1,19 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int mx = 1e5;
+const int mx = 1e6;
+int a[mx];
 
 int main(){
     int n;
-    cin>>n;                                 //5
+    cin>>n;                          
     
-    int a[mx];
-    for(int i=0; i<n; i++){
-        cin>>a[i];                          //1 2 3 4 5
+    for(int i=1; i<=n; i++){
+        cin>>a[i];                   
     }
     
-    for(int i=n-1; i>=0; i--){
-        cout<<a[i]<<' ';                     //5 4 3 2 1
+    for(int i=1, j=n; i<j; i++,j--){   
+        int temp = a[i];
+        a[i]=a[j];
+        a[j]=temp;
     }
-    return 0;
+    
+    for(int i = 1; i<= n; i++){
+        cout<<a[i]<<' ';
+    }
+  return 0;  
 }
