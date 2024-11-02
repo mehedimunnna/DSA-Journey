b440427672eb0d5505389c219cdcfac34312ee57
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-11-02 16:52:59 +0600

    Create 1.vector_basic.cpp

---
+++ Vector/1.vector_basic.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(2);                                           //pushback
+    v.push_back(4);
+    v.push_back(6);
+    v.push_back(8);
+    v.push_back(10);
+    v.push_back(12);
+    v.push_back(14);
+    v.push_back(16);
+    v.push_back(18);
+    v.push_back(20);
+    v.push_back(22);
+    v.push_back(24);
+
+    cout << "Size = " << v.size() << endl;                     //size()
+
+    for (int i = 0; i<v.size(); i++){
+        cout << v.at(i) << endl;                               //print
+    }
+
+    cout <<"Front = "<< v.front() << endl;                     //front value
+    cout <<"Back = "<< v.back() << endl;                       //back value
+
+    if(v.empty()){                                             //empty
+        cout << "Empty"<<endl;
+    }
+    else{
+        cout << "Not Empty"<<endl;
+    }
+
+    v.pop_back();
+    cout << "Last value delete: ";                             // pop back
+    for (int i = 0; i<v.size(); i++){
+        cout << v.at(i)<<' ';                                  // print
+    }
+
+    cout << endl;
+
+    v.erase(v.begin() + 5,v.begin()+7);                        //erase value
+    cout << "Erase: ";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v.at(i)<<' ';
+    }
+
+    cout << endl;
+
+    v.clear();
+     cout << "Size = " << v.size() << endl;                     //clear
+
+}
