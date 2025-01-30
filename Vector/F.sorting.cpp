810c2b7c153999aa838bf56f7867e24a93a91b2d
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-01-30 13:49:00 +0600

    F.sorting.cpp

--- Vector/F.sorting.cpp
+++ Vector/F.sorting.cpp
@@ -1,30 +1,28 @@
-//Increaing order
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     vector<int>v;
     int n;
-    cin>>n;                                             //5
-    cout<<"Input Values: ";
+    cin>>n;
+                                                 
     for(int i = 0; i<n; i++){
         int x;
         cin>>x;
         v.push_back(x);                                //30 20 50 10 65
-    }
-    cout<<endl;
-    
-    cout<<"Before Sorting: ";
-    for(int i = 0; i<v.size();i++){
-        cout<<v[i]<<" ";                                //30 20 50 10 65                
-    }
+    } 
+
+    //Increasing
     sort(v.begin(),v.end());
+    for(auto u:v){
+      cout<<u<<" ";                                    //10 20 30 50 65                       
+    }
     cout << endl;
-    
-    cout<<"After Sorting: ";
-    for(int i = 0; i<v.size();i++){
-        cout<<v[i]<<" ";                                //10 20 30 50 65             
+
+    //Decreasing
+    sort(v.rbegin(),v.rend());
+    for(auto u:v){
+      cout<<u<<" ";                                    //65 50 30 20 10                       
     }
-    sort(v.begin(),v.end());
     cout << endl;
 }
