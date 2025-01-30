58b3035c5774faa553142311f16afc7f25ecb857
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-01-30 12:20:06 +0600

    Create 10.take_input_untill_0.cpp

---
+++ Vector/10.take_input_untill_0.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  vector<int>v;
+
+  while(cin>>n){
+    if(n==0) 
+      break;
+        v.push_back(n);
+  }
+  cout<<"Total elements: ";
+  cout<<v.size()<<endl;
+  for(auto u:v){
+    cout<<u<<" ";
+    cout<<endl;
+  }
+}
