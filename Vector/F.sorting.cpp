8ad0417e215d513944cfca8f5e430b3b70dec798
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-08-15 21:10:03 +0600

    F.sorting.cpp

--- Vector/F.sorting.cpp
+++ Vector/F.sorting.cpp
@@ -1,35 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<int>v;
-    int n;
-    cin>>n;
-                                                 
-    for(int i = 0; i<n; i++){
-        int x;
-        cin>>x;
-        v.push_back(x);                                //30 20 50 10 65
-    } 
+int main() {
+    int n; 
+    cin >> n;
+    vector<int> v(n);
+    for (int &x : v) cin >> x;
 
-    //Increasing
-    sort(v.begin(),v.end());
-    for(auto u:v){
-      cout<<u<<" ";                                    //10 20 30 50 65                       
-    }
-    cout << endl;
+    sort(v.begin(), v.end());           // Increasing
+    for (int x : v) cout << x << " "; 
+    cout << "\n";
 
-    //Decreasing
-    sort(v.rbegin(),v.rend());
-    for(auto u:v){
-      cout<<u<<" ";                                    //65 50 30 20 10                       
-    }
-    cout << endl;
+    sort(v.rbegin(), v.rend());         // Decreasing
+    for (int x : v) cout << x << " "; 
+    cout << "\n";
 
-    //sort till index 3
-    sort(v.begin(),v.begin()+3);
-    for(auto u:v){
-      cout<<u<<" ";                                   //30 50 65 20 10 
-    }
-    cout << endl;
+    sort(v.begin(), v.begin() + min(5, n));  // Sort till index 4
+    for (int x : v) cout << x << " "; 
 }
