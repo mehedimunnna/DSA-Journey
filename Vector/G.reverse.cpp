6ee3023fc6c442fb5b3d5d3d07fd9bf595a9a8ed
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-08-16 01:51:51 +0600

    Update G.reverse.cpp

--- Vector/G.reverse.cpp
+++ Vector/G.reverse.cpp
@@ -1,20 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<int>v;
-    int n;
-    cin>>n;                                             //5
-
-    for(int i = 0; i<n; i++){
-        int x;
-        cin>>x;
-        v.push_back(x);                                //10 20 30 40 50
-    }
+int main() {
+    int n;  cin >> n;
+    vector<int> v(n);
     
-    reverse(v.begin(),v.end());
-    for(auto u:v){
-      cout<<u<<" ";                                    //65 10 50 20 30 
-    }
-    cout<<endl;
+    for (int &x : v) cin >> x;   
+
+    reverse(v.begin(), v.end()); 
+
+    for (int x : v) cout << x << " "; 
 }
