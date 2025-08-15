c23745b0e5ed2eb7710dedfe1cccd7f07393acb0
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2025-08-15 21:11:01 +0600

    Update E.swaping.cpp

--- Vector/E.swaping.cpp
+++ Vector/E.swaping.cpp
@@ -1,39 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<int>v1;
-    int n;
-    cin>>n;                                             //5
-    cout<<"Before swapping V1; ";
-    for(int i = 0; i<n; i++){
-        int x;
-        cin>>x;
-        v1.push_back(x);                                //1 2 3 4 5
-    }
-    cout<<endl;
-    
-    vector<int>v2;
-    int m;
-    cin>>m;
-    cout<<"Before swapping V2; ";                       //5
-    for(int i = 0; i<n; i++){
-        int x;
-        cin>>x;
-        v2.push_back(x);                                //6 7 8 9 10
-    }
-    cout<<endl;
-    
-    swap(v1,v2);
-    cout<<"After swap V1 : ";
-    for(int i = 0; i<v1.size();i++){
-        cout<<v1[i]<<" ";                               //6 7 8 9 10
-    }
-    cout<<endl;
-    
-    cout<<"After swap V2 : ";
-    for(int i = 0; i<v2.size();i++){
-        cout<<v2[i]<<" ";                               //1 2 3 4 5
-    }
-    cout << endl;
+int main() {
+    int n, m; 
+    cin >> n; vector<int> v1(n); for (int &x : v1) cin >> x;
+    cin >> m; vector<int> v2(m); for (int &x : v2) cin >> x;
+    v1.swap(v2);
+    for (int x : v1) cout << x << " "; cout << "\n";
+    for (int x : v2) cout << x << " ";
 }
+
